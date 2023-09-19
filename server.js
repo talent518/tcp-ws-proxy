610d6f43b606ea3cab6fe368b2c3c10bae540c75
阿宝 <parnter_zhangbaocai@saicmotor.com>
2023-09-19 20:20:26 +0800

    tcp基于websocket代理。

---
+++ server.js
@@ -0,0 +1,143 @@
+const net = require('net');
+
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const Layer = require('express/lib/router/layer');
+const cookieParser = require("cookie-parser");
+const bodyParser = require('body-parser');
+const WS = require('express-ws');
+const compression = require('compression');
+const { isAsyncFunction } = require('util/types');
+
+const app = express();
+WS(app);
+app.use(compression());
+app.set('json spaces', 4);
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false, limit: '16mb' }));
+app.use(bodyParser.json({ limit: '16mb' }));
+
+const wsConns = {};
+
+app.get('/', (req, res)=>{
+	res.type('application/json');
+	res.send(Object.keys(wsConns).map(addr=>{
+		return {addr, conns: wsConns[addr].wses};
+	}));
+});
+
+app.ws('/ws/:addr', (ws, req)=>{
+	const addr = req.params.addr;
+	let wsConn;
+	if(addr in wsConns) {
+		wsConn = wsConns[addr];
+	} else {
+		const addrs = addr.split(':');
+		const host = addrs[0];
+	    const port = parseInt(addrs[1]);
+		const sock = new net.Socket();
+
+		wsConns[addr] = wsConn = {wses: [], sock};
+    
+	    sock.setEncoding('utf-8');
+		sock.connect(port, host, function() {
+			console.log('Connected to ' + host + ':' + port + ' success');
+		});
+		sock.on('data', function(data) {
+			// process.stdout.write(data);
+			wsConn.wses.forEach(c=>c.send(data));
+		});
+		sock.on('error', function(err) {
+			console.log('Error(' + host + ':' + port + '): ' + err.message);
+		});
+		sock.on('close', function() {
+			delete wsConn.sock;
+
+			console.log('Closed to ' + host + ':' + port + '\n');
+
+			wsConn.wses.forEach(ws=>ws.close());
+
+			sock.destroy();
+
+			delete wsConns[addr];
+		});
+	}
+	wsConn.wses.push(ws);
+
+	ws.on('message', function(data) {
+		// process.stdout.write(data);
+		if(wsConn.sock) wsConn.sock.write(data);
+	});
+
+	ws.on('close', function() {
+		const i = wsConn.wses.indexOf(ws);
+		wsConn.wses.splice(i, 1);
+		if(!wsConn.wses.length) {
+			if(wsConn.sock) {
+				wsConn.sock.destroy();
+				delete wsConn.sock;
+			}
+		}
+	});
+});
+
+app.use(function (err, req, res, next) {
+	if (err) {
+		console.error(err);
+
+		const message = err.message || '异常错误';
+		if (req.headers['x-requested-with'] === 'XMLHttpRequest' || req.url.startsWith('/api/')) {
+			res.json({ status: false, message, data: config.isDev ? err.stack : [] });
+		} else {
+			res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+			res.status(500);
+			res.send(config.isDev ? err.stack : message);
+		}
+	} else {
+		next();
+	}
+});
+
+const HTTP_PORT = 3004;
+const httpServer = http.createServer(app);
+WS(app, httpServer);
+httpServer.listen(HTTP_PORT, '0.0.0.0');
+httpServer.on('error', console.error);
+httpServer.on('listening', function() {
+	console.log('Listen port is', HTTP_PORT);
+});
+
+Layer.prototype.handle_request = function (req, res, next) {
+	const fn = this.handle;
+
+	if (fn.length > 3) {
+		// not a standard request handler
+		return next();
+	}
+
+	if (isAsyncFunction(fn)) {
+		fn(req, res, next).then(r => {
+			if(r === undefined) return;
+			
+			// console.log(r);
+			
+			if(typeof(r) === 'string') {
+				res.send(r);
+			} else {
+				res.json(r);
+			}
+		}).catch(e => {
+			next(e);
+		});
+	} else {
+		try {
+			fn(req, res, next);
+		} catch (err) {
+			next(err);
+		}
+	}
+};
+
+process.on('uncaughtException', console.error);
